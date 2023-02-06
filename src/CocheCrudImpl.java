bcce340bf6dca025d773ae3a76a1d47236a41631
ErickBrayan <ebrayan2000@gmail.com>
2023-02-05 23:03:14 -0500

    commit Ejercicios 12 13 14

--- src/CocheCrudImpl.java
+++ src/CocheCrudImpl.java
@@ -1,14 +1,26 @@
 public class CocheCrudImpl implements CocheCRUD{
+
+
+    /**
+     * Guarda un Coche
+     */
     @Override
     public void save() {
         System.out.println("Metodo guardar un coche");
     }
 
+    /**
+     * Imprime todos los Coches
+     */
     @Override
     public void findAll() {
         System.out.println("Metodo Recuperar todos los coches");
     }
 
+
+    /**
+     * Elimina un Coche
+     */
     @Override
     public void delete() {
         System.out.println("Metodo eliminar un coche");
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 
+
     static CocheCRUD cocheCRUD = new CocheCrudImpl();
     public static void main(String[] args) {
         cocheCRUD.save();
